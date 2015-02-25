facee2e5b8b54b75854b4948efd697e1fb3d1fba
Washington Hua <ideal19920402@gmail.com>
2015-02-25 23:57:07 +0800

    visual style updated.
    visual style updated.
    1) applying card design.
    2) new banner image.
    3) init for blog.

--- js/index.js
+++ js/index.js
@@ -1,35 +1,40 @@
 window.onload = init();
 
+var BASE_LOCATION = "http://tonghuashuo.github.io";
+
 function init() {
 	var mf_sidebar 	= document.getElementById("mf_sidebar");
 	var mf_portrait = document.getElementById("mf_portrait");
 	var mf_ghid 	= document.getElementById("mf_ghid");
 	var mf_category = document.getElementById("mf_category");
 	
+	mf_portrait.onclick = function() {
+		window.location.href = BASE_LOCATION;
+	}
+
+	// console.log("calling goMobile() on init");
 	goMobile();
 
-	window.onresize = goMobile();
+	window.onresize = function() {
+		// console.log("calling goMobile() on resize");
+		goMobile();
+	}
 
 	function goMobile() {
-		console.log("goMobile() called");
+		// console.log("goMobile() called");
 		if (window.innerWidth >960) {
 			// PC Style
 			mf_sidebar.style.height = window.innerHeight+"px";
 		} else {
 			// Mobile Style
 			mf_sidebar.style.height = "48px";
-			// console.log(window.innerWidth);
-			// console.log(getCurrentStyle(mf_sidebar)['height']);
-			// mf_category.style.width = (window.innerWidth - getCurrentStyle(mf_sidebar)['height']) + "px";
-			// console.log("mf_category.style.width: " + mf_category.style.width);
-			// console.log("getCurrentStyle(mf_category)['width']: " + getCurrentStyle(mf_category)['width']);
-			// console.log(mf_category.getElementsByTagName("li"));
-			// alert(mf_category.getElementsByTagName("li"));
 		}
 	}
 
-	// node is the DOM element whose CSS style you wanna retrive
-	// function returns a key-value set of computed CSS properties of the given node
+	/** 
+	 * node is the DOM element to get style from
+	 * returns a key-value set of computed style
+	 */
 	function getCurrentStyle(node) {
 	    var style = null;
 	    
