4a5873f5663bead68be0b210c4e3cd2c2aefec71
Washington Hua <ideal19920402@gmail.com>
2015-03-02 15:27:25 +0800

    innit seo, using webp
    1) added meta data for SEO
    2) convert all image resources into WebP
    3) added console trick

--- js/index.js
+++ js/index.js
@@ -3,6 +3,16 @@ window.onload = init();
 var BASE_LOCATION = "http://tonghuashuo.github.io";
 
 function init() {
+	console.log("================================");
+	console.log("Aha!");
+	console.log("You found me !");
+	console.log("There are only 10 people in this");
+	console.log("world know i'm here. You are one");
+	console.log("of them. That makes us friends. ");
+	console.log("here: ideal19920402@gmail.com. ");
+	console.log("Contact me via that. ");
+	console.log("================================");
+
 	var mf_sidebar 	= document.getElementById("mf_sidebar");
 	var mf_portrait = document.getElementById("mf_portrait");
 	var mf_ghid 	= document.getElementById("mf_ghid");
