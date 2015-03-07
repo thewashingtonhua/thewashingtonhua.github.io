48a1db354e93058e49cbba3622f9a1ad7c9e2d03
Washington Hua <ideal19920402@gmail.com>
2015-03-08 01:11:29 +0800

    gh-page V1.0 MileStone
    1) gh-page V1.0 finished
    2) new blog about stack calculator

--- js/index.js
+++ js/index.js
@@ -15,27 +15,44 @@ function init() {
 	var mf_portrait = document.getElementById("mf_portrait");
 	var mf_ghid 	= document.getElementById("mf_ghid");
 	var mf_category = document.getElementById("mf_category");
-	
+
+	var friend_card = document.getElementById("mf_content").childNodes;
+
 	mf_portrait.onclick = function() {
 		window.location.href = BASE_LOCATION;
 	}
 
-	// console.log("calling goMobile() on init");
 	goMobile();
 
 	window.onresize = function() {
-		// console.log("calling goMobile() on resize");
 		goMobile();
 	}
 
 	function goMobile() {
-		// console.log("goMobile() called");
-		if (window.innerWidth >960) {
+		if (window.innerWidth > 960) {
 			// PC Style
 			mf_sidebar.style.height = window.innerHeight+"px";
 		} else {
 			// Mobile Style
 			mf_sidebar.style.height = "48px";
+			if(window.innerWidth <= 630) {
+				if(friend_card[3] && friend_card[3].className && friend_card[3].className == "card friend") {
+					for (var i=3; i<friend_card.length - 1; i+=2) {
+						friend_card[i].style.width = (window.innerWidth - 30) + "px";
+						friend_card[i].childNodes[1].style.width = (window.innerWidth - 30) + "px";
+						friend_card[i].childNodes[7].childNodes[0].style.width = (window.innerWidth - 30) + "px";
+						console.log(friend_card[i].childNodes[7].childNodes[0]);
+					}
+				}
+			} else {
+				if(friend_card[3] && friend_card[3].className && friend_card[3].className == "card friend") {
+					for (var i=3; i<friend_card.length - 1; i+=2) {
+						friend_card[i].style.width = "280px";
+						friend_card[i].childNodes[1].style.width = "280px";
+						friend_card[i].childNodes[7].childNodes[0].style.width = "280px";
+					}
+				}
+			}
 		}
 	}
 
