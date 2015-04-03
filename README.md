cdebafc7d1105fc7fc28426f5e04a298e462176d
Washington Hua <ideal19920402@gmail.com>
2015-04-03 23:40:29 +0800

    Use JS to load Main Frame
    Use JavaScript to load navigation Bar and Footer
    Multi-language recognition according to HTML lang property

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
->##Attention !!!!!!
->For those who want EN version. change the 'cn' in the url into 'en' and you're good to go. I'll put a switch button later. But now my bed is calling me.
-
 #Major Change Log
 
-## 2015-03-26
+### 2015-04-03
+- Use JavaScript to load navigation Bar and Footer
+- Multi-language recognition according to HTML lang property
+
+### 2015-03-26
 - Added English Version
 
-## 2015-03-08 ( V1.0 release )
+### 2015-03-08 ( V1.0 release )
 - Flat Design : Card-Style, Roboto, thin font-weight.
 - Responsive Design, mobile Friendly.
 - Uploaded 2 blogs.
--- js/index.js
+++ js/index.js
@@ -3,15 +3,10 @@ window.onload = init;
 var BASE_LOCATION = "http://tonghuashuo.github.io";
 
 function init() {
-	/*console.log("================================");
-	console.log("Aha!");
-	console.log("You found me !");
-	console.log("There are only 10 people in this");
-	console.log("world know i'm here. You are one");
-	console.log("of them. That makes us friends. ");
-	console.log("If you gonna contact me. Tell me");
-	console.log("you found me from console.      ");
-	console.log("================================");*/
+	
+	printConsoleGreetings();
+	initNavigator();
+	initFooter();
 
 	var mf_sidebar 	= document.getElementById("mf_sidebar");
 	var mf_portrait = document.getElementById("mf_portrait");
@@ -19,20 +14,6 @@ function init() {
 	var mf_category = document.getElementById("mf_category");
 
 	var card_waterfall = document.getElementById("mf_content").childNodes;
-
-	mf_portrait.onclick = function() {
-		if (document.getElementsByTagName('html')[0].hasAttribute("lang")) {
-			var lang = document.getElementsByTagName('html')[0].getAttribute("lang");
-			if(lang == "zh-CN") {
-				window.location.href = BASE_LOCATION + "/cn/blog.html";
-			} else {
-				window.location.href = BASE_LOCATION + "/en/blog.html";
-			}
-		} else {
-			alert("attribute 'lang' not found");
-			window.location.href = BASE_LOCATION;
-		}
-	}
 
 	goMobile();
 	fixTitleHeight();
@@ -74,7 +55,9 @@ function init() {
 	function fixTitleHeight() {
 		var blogs = document.getElementsByClassName("blog");
 		var banner = document.getElementsByClassName("banner")[0];
-		if (banner) var bannerHeight = Number(getCurrentStyle(banner)["height"].slice(0, -2));
+		// The following codes only work for blog category.
+		// Only blog category has banner.
+		if(banner) var bannerHeight = Number(getCurrentStyle(banner)["height"].slice(0, -2));
 
 		for (var i=0, len=blogs.length; i<len; i++) {
 		// for (var i=0; i<1; i++) {
@@ -107,6 +90,20 @@ function init() {
 			// console.log("margin-top: " + title.style.marginTop);
 		}
 	}
+
+	mf_portrait.onclick = function() {
+		var lang = getLang();
+		if(lang) {
+			if(lang == "zh-CN") {
+				window.location.href = BASE_LOCATION + "/cn/blog.html";
+			} else {
+				window.location.href = BASE_LOCATION + "/en/blog.html";
+			}
+		} else {
+			window.location.href = BASE_LOCATION;
+			console.err("attribute 'lang' not found");
+		}
+	}
 }
 
 /** 
@@ -123,4 +120,147 @@ function getCurrentStyle(node) {
     }
     
     return style;
+}
+
+function getLang() {
+	if (document.getElementsByTagName('html')[0].hasAttribute("lang")) {
+		return document.getElementsByTagName('html')[0].getAttribute("lang");
+	}
+	return null;
+}
+
+function printConsoleGreetings() {
+	console.log("================================");
+	console.log("Aha! You found me !");
+	console.log("There are only 10 people in this");
+	console.log("world know i'm here. You are one");
+	console.log("of them. That makes us friends. ");
+	console.log("If you gonna contact me. Tell me");
+	console.log("you found me in the console.    ");
+	console.log("================================");
+}
+
+function initNavigator() {
+	var new_mf_sidebar = document.createElement("div");
+	var new_mf_profile = document.createElement("div");
+	var new_mf_portrait = document.createElement("div");
+	var new_mf_ghid = document.createElement("a");
+	var new_mf_category = document.createElement("ul");
+	var blog_li = document.createElement("li");
+	var project_li = document.createElement("li");
+	var friend_li = document.createElement("li");
+	var about_li = document.createElement("li");
+	var blog_a = document.createElement("a");
+	var project_a = document.createElement("a");
+	var friend_a = document.createElement("a");
+	var about_a = document.createElement("a");
+
+	new_mf_sidebar.id = "mf_sidebar";
+	new_mf_profile.id = "mf_profile";
+	new_mf_portrait.id = "mf_portrait";
+	new_mf_ghid.id = "mf_ghid";
+	new_mf_ghid.href = "https://github.com/tonghuashuo";
+	new_mf_ghid.target = "_blank";
+	new_mf_ghid.innerHTML = "@tonghuashuo";
+	new_mf_category.id = "mf_category";
+	blog_li.id = "blog";
+	project_li.id = "project";
+	friend_li.id = "friend";
+	about_li.id = "about";
+
+	new_mf_profile.appendChild(new_mf_portrait);
+	new_mf_profile.appendChild(new_mf_ghid);
+
+	blog_li.appendChild(blog_a);
+	project_li.appendChild(project_a);
+	friend_li.appendChild(friend_a);
+	about_li.appendChild(about_a);
+	new_mf_category.appendChild(blog_li);
+	new_mf_category.appendChild(project_li);
+	new_mf_category.appendChild(friend_li);
+	new_mf_category.appendChild(about_li);
+
+	new_mf_sidebar.appendChild(new_mf_profile);
+	new_mf_sidebar.appendChild(new_mf_category);
+
+	var new_mf_content = document.getElementById("mf_content");
+
+	document.body.insertBefore(new_mf_sidebar, new_mf_content);
+
+	var lang = getLang();
+	var url = window.location.href;
+	// console.log("lang: " + lang);
+	// console.log("url: " + url);
+	var base = url.indexOf("tonghuashuo.github.io");
+	var sub = url.substr(base).split("/");
+	var level = sub.length - 3;
+	// console.log("sub: " + sub);
+	// console.log("level: " + level);
+
+	var sub_str = "";
+	for(var i=0; i<level; i++) {
+		sub_str += "../";
+	}
+
+	blog_a.href = sub_str + "blog.html";
+	project_a.href = sub_str + "project.html";
+	friend_a.href = sub_str + "friend.html";
+	about_a.href = sub_str + "about.html";
+	
+	if(lang == "zh-CN") {
+		blog_a.innerHTML = "博客";
+		project_a.innerHTML = "项目";
+		friend_a.innerHTML = "朋友";
+		about_a.innerHTML = "关于";	
+	} else {
+		blog_a.innerHTML = "blog";
+		project_a.innerHTML = "project";
+		friend_a.innerHTML = "friend";
+		about_a.innerHTML = "about";	
+	}
+
+	if(level) {
+		var channel = sub[2];
+	} else {
+		var channel = sub[2].slice(0, -5);
+	}
+	// console.log("channel: " + channel);
+	// console.log(document.getElementById(channel));
+	document.getElementById(channel).className = "active";
+}
+
+function initFooter() {
+	var footer = document.createElement("footer");
+
+	footer.innerHTML = '<p><a id="toCN">简体中文</a>|<a id="toEN">English</a></p><p><a target="_blank" href="http://jigsaw.w3.org/css-validator/check/referer"><img style="border:0;width:88px;height:31px" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="Valid CSS!" /></a></p>';
+	document.getElementById("mf_content").appendChild(footer);
+
+	var url = window.location.href;
+	var base = url.indexOf("tonghuashuo.github.io");
+	var sub = url.substr(base).split("/");
+	var level = sub.length;
+
+	// console.log("url: " + url);
+	// console.log("sub: " + sub);
+	// console.log("level: " + level);
+
+	var sub_str = "";
+	for(var i=2; i<level; i++) {
+		sub_str += "../";
+	}
+
+	var toCN = document.getElementById("toCN");
+	var toEN = document.getElementById("toEN");
+
+	var target_cn = sub_str + "cn";
+	var target_en = sub_str + "en";
+	for(var i=2; i<level; i++) {
+		target_cn += "/" + sub[i];
+		target_en += "/" + sub[i];
+	}
+	// console.log(target_cn);
+	// console.log(target_en);
+
+	toCN.href = target_cn;
+	toEN.href = target_en;
 }
