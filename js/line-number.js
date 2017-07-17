a700da56ab80aeac58208db70b42335e19820fe0
Washington Hua <ideal19920402@gmail.com>
2017-07-17 14:41:29 +0800

    lint with standard, and add baidu tongji/zhanzhang code

--- js/line-number.js
+++ js/line-number.js
@@ -1,13 +1,14 @@
-$(function(){
-    $('pre code').each(function(){
-        var lines = $(this).text().split('\n').length;
-        var $numbering = $('<ul/>').addClass('line-number');
-        $(this)
-            .addClass('has-line-number')
-            .parent()
-            .append($numbering);
-        for(var i=1; i<=lines; i++){
-            $numbering.append($('<li/>').text(i));
-        }
-    });
-});
+/* eslint-disable no-undef */
+$(function () {
+  $('pre code').each(function () {
+    var lines = $(this).text().split('\n').length
+    var $numbering = $('<ul/>').addClass('line-number')
+    $(this)
+      .addClass('has-line-number')
+      .parent()
+      .append($numbering)
+    for (var i = 1; i <= lines; i++) {
+      $numbering.append($('<li/>').text(i))
+    }
+  })
+})
