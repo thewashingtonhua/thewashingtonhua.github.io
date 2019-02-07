785bfcb5873299c32ad2ca4dc5a984c2f40cac27
Washington Hua <ideal19920402@gmail.com>
2019-02-07 13:29:11 +0800

    chore: cleanup

--- config/concat.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  base: require('./concat/base.config.js'),
-  blog: require('./concat/blog.config.js'),
-  project: require('./concat/project.config.js'),
-  lab: require('./concat/lab.config.js')
-}
