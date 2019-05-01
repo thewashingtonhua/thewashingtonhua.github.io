18d6facd045a607d75727a250c8d34504f5705cf
Washington Hua <ideal19920402@gmail.com>
2019-05-01 15:49:04 +0800

    feat: integrate Sentry.io

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import Header from './header'
+import * as Sentry from '@sentry/browser'
 import 'normalize-scss/sass/normalize/_import-now.scss'
 import './layout.scss'
+import Header from './header'
+
+Sentry.init({
+  dsn: 'https://9638de4372be4acebf892d0732a86a4a@sentry.io/1450204'
+})
 
 const Layout = ({ children }) => (
   <StaticQuery
